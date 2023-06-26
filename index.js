8419b13c83a8f31be11d8273403de5c7656de9b7
sourabhongit <get.sourabhdas@gmail.com>
2023-06-26 22:34:10 +0530

    added env and port

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+require("dotenv").config(); // for .env file
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.listen("5000", (err) => {
+app.listen("process.env.PORT || 5000", (err) => {
   if (!err) {
-    console.log("Server is running on port 5000");
+    console.log("Server is running");
   } else {
     console.log(err);
   }
